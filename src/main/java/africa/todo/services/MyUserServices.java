936479dba98b2d15be416c248425a84e78123881
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-07 16:27:08 +0100

    java

---
+++ src/main/java/africa/todo/services/MyUserServices.java
@@ -0,0 +1,8 @@
+package africa.todo.services;
+
+import africa.todo.dataTransferObjects.RegisterRequest;
+
+public interface MyUserServices {
+    void register(RegisterRequest registerRequest);
+
+}

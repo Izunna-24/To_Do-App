851a1c52815a9b7b72748c0a713ab765821d6e09
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-08 08:28:14 +0100

    java

--- src/test/java/africa/todo/data/repositories/UserRepositoryTest.java
+++ src/test/java/africa/todo/data/repositories/UserRepositoryTest.java
@@ -1,23 +1,23 @@
 package africa.todo.data.repositories;
 
-import africa.todo.data.models.MyUser;
+import africa.todo.data.models.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DataMongoTest
-class MyUserRepositoryTest {
+class UserRepositoryTest {
     @Autowired
-    private MyUserRepository myUserRepository;
+    private UserRepository userRepository;
 
 
     @Test
     public void myUserCanBeSavedInTheDBTest(){
-        myUserRepository.deleteAll();
-        MyUser myUser = new MyUser();
-        myUserRepository.save(myUser);
-        assertEquals(1,myUserRepository.count());
+        userRepository.deleteAll();
+        User user = new User();
+        userRepository.save(user);
+        assertEquals(1, userRepository.count());
     }
 
 }

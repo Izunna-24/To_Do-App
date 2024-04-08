32b8cef4b92fc42c5712f75580a4b452c9291f0d
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-08 08:27:50 +0100

    java

---
+++ src/main/java/africa/todo/data/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package africa.todo.data.repositories;
+
+import africa.todo.data.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findById(String id);
+
+    boolean existsByUsername(String username);
+}

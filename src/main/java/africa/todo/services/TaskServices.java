f41a1072f3c0615ef5b2e0c059a84263b3aa8088
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-10 08:42:21 +0100

    java

--- src/main/java/africa/todo/services/TaskServices.java
+++ src/main/java/africa/todo/services/TaskServices.java
@@ -1,7 +1,9 @@
 package africa.todo.services;
 
 import africa.todo.dataTransferObjects.requests.CreateTaskRequest;
+import africa.todo.dataTransferObjects.requests.ViewTaskRequest;
 
 public interface TaskServices {
     void createTask(CreateTaskRequest createTaskRequest);
+    void viewTask(ViewTaskRequest viewTaskRequest);
 }

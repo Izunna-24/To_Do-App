107b206bed47aef880cf8f06ab89901f7c67be70
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-10 08:41:26 +0100

    java

--- src/main/java/africa/todo/data/repositories/TaskRepository.java
+++ src/main/java/africa/todo/data/repositories/TaskRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface TaskRepository extends MongoRepository<Task,String> {
-Optional<Task> findById(String id);
+Optional<Task> findByTaskId(String id);
 Optional<Task> findByTaskName(String taskName);
 }

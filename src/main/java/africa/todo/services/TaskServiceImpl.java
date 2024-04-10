9b69479a054e4330a8260ec3b2010a7e6d82c0c8
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-10 08:41:51 +0100

    java

--- src/main/java/africa/todo/services/TaskServiceImpl.java
+++ src/main/java/africa/todo/services/TaskServiceImpl.java
@@ -5,8 +5,13 @@ import africa.todo.data.models.Task;
 import africa.todo.data.models.User;
 import africa.todo.data.repositories.TaskRepository;
 import africa.todo.dataTransferObjects.requests.CreateTaskRequest;
+import africa.todo.dataTransferObjects.requests.ViewTaskRequest;
+import africa.todo.exceptions.TaskNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class TaskServiceImpl implements TaskServices{
@@ -17,7 +22,16 @@ public class TaskServiceImpl implements TaskServices{
         Task task = new Task();
         task.setTaskName(createTaskRequest.getTaskName());
         task.setContent(createTaskRequest.getContent());
-        task.setTaskDateTime();
+        task.setTaskDateTime(LocalDateTime.now());
+        taskRepository.save(task);
 
     }
+
+    @Override
+    public void viewTask(ViewTaskRequest viewTaskRequest) {
+        Optional<Task> task = taskRepository.findByTaskId(viewTaskRequest.getId());
+        if (!taskRepository.existsById(viewTaskRequest.getId())) throw new TaskNotFoundException("Task was never created");
+    }
+
+
 }
